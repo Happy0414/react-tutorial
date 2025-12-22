a461c591716011ddfca8801a3065d5685fe283b3
Ayumu Fujimoto <fujimotoayumu0414@icloud.com>
2025-12-22 18:05:46 +0900

    chakraUIの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button, ChakraProvider, Text } from '@chakra-ui/react'
 import './App.css'
 import { Title } from './Title'
 
 function App() {
   return (
+    <ChakraProvider>
     <div>
-      <Title />
-      <Title />
-      <Title />
+      <Text fontSize="2x1">家計簿アプリ</Text>
     </div>
+    </ChakraProvider>
+    
   )
 }
 
