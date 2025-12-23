af177b48f51550ddffa58fa9b3f70b96dc0aa523
Ayumu Fujimoto <fujimotoayumu0414@icloud.com>
2025-12-24 04:13:19 +0900

    入力フォームの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Button, ChakraProvider, Text } from '@chakra-ui/react'
+import { Button, ChakraProvider, Text, Checkbox, Flex, Input} from '@chakra-ui/react'
 import './App.css'
 import { Title } from './Title'
 
@@ -10,6 +10,13 @@ function App() {
     <ChakraProvider>
     <div>
       <Text fontSize="2x1">家計簿アプリ</Text>
+      <div>
+        <Input placeholder='支出を入力' mb="4px" />
+        <Flex align="center" justifyContent="space-between">
+          <Checkbox w="100px">入金</Checkbox>
+          <Button colorScheme="teal">追加</Button>
+        </Flex>
+      </div>
     </div>
     </ChakraProvider>
     
